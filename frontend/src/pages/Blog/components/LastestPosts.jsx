9ccdb80c5d3ef5d5a559fefd2d13cc88a29f04b5
Phung Duc Long <phungduclong268@gmail.com>
2026-01-21 16:41:12 +0700

    deploy

--- frontend/src/pages/Blog/components/LastestPosts.jsx
+++ frontend/src/pages/Blog/components/LastestPosts.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import ShowBlog from '../../../components/layout/ShowBlog';
+import React from "react";
+import ShowBlog from "../../../components/layout/ShowBlog";
 
 const LastestPosts = ({ blogs }) => {
-    return (
-        <>
-            <div className="py-2.5 mb-12.5 text-2xl border-b border-gray-100">
-                <h2 className="my-3.75">Latest Posts</h2>
-            </div>
+  return (
+    <>
+      <div className="py-2.5 mb-12.5 text-2xl border-b border-gray-100">
+        <h2 className="my-3.75">Latest Posts</h2>
+      </div>
 
-            <div>
-                <ShowBlog
-                    blog={blogs}
-                    wrapClass="grid grid-cols-[1fr_2fr] gap-8 text-left!"
-                    imageClass="w-[260px]! h-[210px]!"
-                />
-            </div>
-        </>
-    );
+      <div>
+        <ShowBlog
+          blog={blogs}
+          wrapClass="lg:grid grid-cols-[1fr_2fr] gap-8 lg:text-left!"
+          imageClass="w-full lg:w-[260px]! h-[210px]!"
+        />
+      </div>
+    </>
+  );
 };
 
 export default LastestPosts;
